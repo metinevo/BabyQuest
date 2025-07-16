2635d092d316bd32be17b25e99ab7f936baf9a5b
Eric Ehlers <metinevo@proton.me>
2025-07-16 20:34:57 +0200

    player movement

---
+++ godot/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var speed: int = 35
+@onready var animations = $AnimationPlayer
+
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection * speed
+
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "down"
+		if velocity.x < 0: direction = "left"
+		elif velocity.x > 0: direction = "right"
+		elif velocity.y < 0: direction = "up"
+		animations.play("walk_" + direction)
+
+func _physics_process(_delta: float) -> void:
+	handleInput()
+	move_and_slide()
+	updateAnimation()
